9059a081003f791ac9f304e1c773a98db2deede2
JECanelos <juan.canelos@mosaicfactor.com>
2023-10-01 17:19:35 -0500

    add: config demo placeholderData and initialData

--- src/issues/hooks/useLabels.tsx
+++ src/issues/hooks/useLabels.tsx
@@ -17,25 +17,25 @@ export const useLabels = () => {
     () => getLabels(),
     {
       // staleTime: 1000 * 60 * 60, // 1 hour
-      // placeholderData: [
       // initialData: [
-      //   {
-      //     id: 717031390,
-      //     node_id: 'MDU6TGFiZWw3MTcwMzEzOTA=',
-      //     url: 'https://api.github.com/repos/facebook/react/labels/good%20first%20issue',
-      //     name: 'good first issue',
-      //     color: '6ce26a',
-      //     default: true,
-      //   },
-      //   {
-      //     id: 725156255,
-      //     node_id: 'MDU6TGFiZWw3MjUxNTYyNTU=',
-      //     url: 'https://api.github.com/repos/facebook/react/labels/good%20first%20issue%20(taken)',
-      //     name: 'good first issue (taken)',
-      //     color: 'b60205',
-      //     default: false,
-      //   }
-      // ],
+      placeholderData: [
+        {
+          id: 717031390,
+          node_id: 'MDU6TGFiZWw3MTcwMzEzOTA=',
+          url: 'https://api.github.com/repos/facebook/react/labels/good%20first%20issue',
+          name: 'good first issue',
+          color: '6ce26a',
+          default: true,
+        },
+        {
+          id: 725156255,
+          node_id: 'MDU6TGFiZWw3MjUxNTYyNTU=',
+          url: 'https://api.github.com/repos/facebook/react/labels/good%20first%20issue%20(taken)',
+          name: 'good first issue (taken)',
+          color: 'b60205',
+          default: false,
+        }
+      ],
     }
   );
 };
