f30382eff5daa883bf50036ebebc8e53b0c46a0d
JECanelos <juan.canelos@mosaicfactor.com>
2023-10-08 15:44:24 -0500

    add: local env var

--- src/api/githubApi.ts
+++ src/api/githubApi.ts
@@ -3,6 +3,6 @@ import axios from 'axios';
 export const githubApi = axios.create({
   baseURL: 'https://api.github.com/repos/facebook/react',
   headers: {
-    Authorization: `Bearer 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`,
+    Authorization: `Bearer ${import.meta.env.VITE_GITHUB_TOKEN}`,
   },
 });
