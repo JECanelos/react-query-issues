1778223184da951175a35754d3d9d535cba9812f
JECanelos <juan.canelos@mosaicfactor.com>
2023-10-08 20:40:59 -0500

    add: label name badge

--- src/issues/components/IssueItem.tsx
+++ src/issues/components/IssueItem.tsx
@@ -54,6 +54,18 @@ export const IssueItem: FC<Props> = ({ issue }) => {
             #{number} opened 2 days ago by{' '}
             <span className="fw-bold">{user.login}</span>
           </span>
+
+          <div>
+            {issue.labels.map(label => (
+              <span
+                key={label.id}
+                className="badge rounded-pill m-1"
+                style={{ backgroundColor: `#${label.color}`, color: 'black' }}
+              >
+                {label.name}
+              </span>
+            ))}
+          </div>
         </div>
 
         <div className="d-flex align-items-center">
